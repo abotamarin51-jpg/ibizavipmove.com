37e333f7b4fcb3c765cd8856f1aaa14390a09883
abotamarin51-jpg <abotamarin51@gmail.com>
2026-09-13 17:11:47 +0200

    Phase 139: isolate pinned Pillow tooling in CI and deployment

--- phase133_audit.py
+++ phase133_audit.py
@@ -8,7 +8,6 @@ from phase135_audit import run as run_localized_services_routing_audit
 from phase136_audit import run as run_localized_authority_footer_routing_audit
 from phase137_audit import run as run_localized_contact_footer_routing_audit
 from phase138_audit import run as run_localized_wordmark_home_routing_audit
-from phase139_audit import run as run_private_office_image_audit
 
 ROOT = Path('_site')
 CONTACTS = ('contact', 'es/contacto', 'fr/contact', 'de/kontakt', 'ar/contact')
@@ -84,4 +83,3 @@ if __name__ == '__main__':
     run_localized_authority_footer_routing_audit()
     run_localized_contact_footer_routing_audit()
     run_localized_wordmark_home_routing_audit()
-    run_private_office_image_audit()
