4c0353a08d5a6e769c2810aa296107dc530b0235
abotamarin51-jpg <abotamarin51@gmail.com>
2026-09-13 17:57:03 +0200

    Phase 140: run vocabulary enhancement in final pipeline

--- phase133_enhance.py
+++ phase133_enhance.py
@@ -5,6 +5,7 @@ from phase135_enhance import enhance as enhance_localized_services_routing
 from phase136_enhance import enhance as enhance_localized_authority_footer_routing
 from phase137_enhance import enhance as enhance_localized_contact_footer_routing
 from phase138_enhance import enhance as enhance_localized_wordmark_home_routing
+from phase140_enhance import enhance as enhance_reviewed_vocabulary_roles
 
 ROOT = Path('_site')
 CONTACTS = ('contact', 'es/contacto', 'fr/contact', 'de/kontakt', 'ar/contact')
@@ -55,3 +56,4 @@ if __name__ == '__main__':
     enhance_localized_authority_footer_routing()
     enhance_localized_contact_footer_routing()
     enhance_localized_wordmark_home_routing()
+    enhance_reviewed_vocabulary_roles()
